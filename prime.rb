aefc1ccf8ca9e51862f9bdab4774704a1fd49456
jomapormentilla <jomapormentilla@gmail.com>
2020-10-21 18:11:28 -0400

    commit tree 6e75b45304abd059aaee3ed82354f973f4e671f1

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+
+# Given an input number (int), check whether it is evenly 
+# divisible by any prime number between 2 and sqrt(int).
+# If so, then (int) is composite, otherwise it is prime
+
+def prime?( int )
+    arr = (2..int).to_a
+
+    # The .one? method checks if exactly one element returns true. If the number is prime, 
+    # then only the last element "int % num" will have a remainder of 0. Otherwise, it returns false
+
+    arr.one? { |num|
+        int % num == 0
+    }
+end
